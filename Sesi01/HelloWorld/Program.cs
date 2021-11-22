e8a7f836bb83185b2eafc550c967fd9fbdcf5cc7
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-11-22 15:58:46 +0700

    perbaikan sesi 1

--- Sesi01/HelloWorld/Program.cs
+++ Sesi01/HelloWorld/Program.cs
@@ -6,23 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            String name = "";
-            int val1;
-            int val2;
-
-            bool notEmpty = name.Length > 0;
-
-            Console.WriteLine("Isenkkk luas kotaqq");
-            Console.Write("Name : ");
-            name = Console.ReadLine();
-            Console.Write("Panjang : ");
-            val1 = int.Parse(Console.ReadLine());
-            Console.Write("Lebar : ");
-            val2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine();
-            Console.WriteLine($"Empty name : {notEmpty}");
-            Console.WriteLine("Luasnya adalah " + (val1 * val2));
+            Console.WriteLine("Hello World!");
         }
     }
 }
