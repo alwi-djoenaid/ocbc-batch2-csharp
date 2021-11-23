e586938e6b09189439d72a6ea5a83036ecc91902
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-11-23 10:44:03 +0700

    Sesi 3

---
+++ Sesi03/IfElse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, pwd;
+
+            Console.Write("Username : ");
+            name = Console.ReadLine();
+            Console.Write("Password : ");
+            pwd = Console.ReadLine();
+
+            bool nameIsCorrect = name == "ocbc";
+            bool pwdIsCorrect = pwd == "bootcamp";
+
+            if(nameIsCorrect && pwdIsCorrect)
+            {
+                Console.WriteLine("Anda berhasil login");
+            } else {
+                Console.WriteLine("Username atau Password anda salah");
+            }
+        }
+    }
+}
---
+++ Sesi03/Loop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int j = 0;
+
+            for(int i=a; i<20; i++){
+                Console.WriteLine(i);
+            }
+
+            while(a < 20){
+                Console.WriteLine(a++);
+            }
+
+            do{
+                Console.WriteLine("i = {0}", j);
+                j++;
+
+                if(j > 5){
+                    break;
+                }
+            }while(j < 10);
+        }
+    }
+}
