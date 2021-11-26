61c4532c7c877c1b402592d9abeff8e6a703c15f
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-11-26 17:52:30 +0700

    Sesi 10 selesai

---
+++ Sesi09/Tugas09/webApiTest/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using webApiTest.Models;
+
+namespace webApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController: ControllerBase
+    {
+        private EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: api/User
+        [HttpGet(Name = "GetEmployee")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            // return new string[]
+            return _context.GetAllEmployee();
+        }
+
+        // GET: api/User/{id}
+        [HttpGet("{id}", Name = "GetEmployeeById")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItem(String id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            // return new string[]
+            return _context.GetEmployee(id);
+        }
+    }
+}
