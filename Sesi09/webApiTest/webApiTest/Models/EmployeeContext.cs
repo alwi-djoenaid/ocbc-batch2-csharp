b3e29376938966302d4becb40d3b68db56d2e470
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-11-26 11:38:41 +0700

    Sesi 9 selesai

---
+++ Sesi09/webApiTest/webApiTest/Models/EmployeeContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace webApiTest.Models
+{
+    public class EmployeeContext
+    {
+        public string ConnectionString { get; set; }
+
+        public EmployeeContext(string ConnectionString)
+        {
+            this.ConnectionString = ConnectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<EmployeeItem> GetAllEmployee()
+        {
+            List<EmployeeItem> list = new List<EmployeeItem>();
+
+            using(MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM employee", conn);
+
+                using(MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new EmployeeItem()
+                        {
+                            id = reader.GetInt32("id"),
+                            name = reader.GetString("name"),
+                            gender = reader.GetString("gender"),
+                            address = reader.GetString("address"),
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+        public List<EmployeeItem> GetEmployee(string id)
+        {
+            List<EmployeeItem> list = new List<EmployeeItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM employee WHERE id=@id", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new EmployeeItem()
+                        {
+                            id = reader.GetInt32("id"),
+                            name = reader.GetString("name"),
+                            gender = reader.GetString("gender"),
+                            address = reader.GetString("address"),
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ Sesi09/webApiTest/webApiTest/Models/EmployeeItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webApiTest.Models
+{
+    public class EmployeeItem
+    {
+        //private Models.EmployeeContext context;
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+    }
+}
