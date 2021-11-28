2d2eb5cad1b3706ae4e4b525cafab01f6e931941
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-11-28 23:22:45 +0700

    tugas dan sesi 9 selesai

--- Sesi09/Tugas09/webApiTest/Controllers/EmployeeController.cs
+++ Sesi09/Tugas09/webApiTest/Controllers/EmployeeController.cs
@@ -25,8 +25,14 @@ namespace webApiTest.Controllers
         {
             _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
 
-            // return new string[]
-            return _context.GetAllEmployee();
+            var employee = _context.GetAllEmployee();
+
+            if (employee.Count != 0)
+            {
+                return new OkObjectResult(new { Success = true, employee });
+            }
+
+            return BadRequest("Failed to get employee records");
         }
 
         // GET: api/User/{id}
@@ -35,8 +41,49 @@ namespace webApiTest.Controllers
         {
             _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
 
+            var employee = _context.GetEmployee(id);
+
+            if(employee.Count == 1)
+            {
+                return new OkObjectResult(new { Success = true, employee });
+            }
+
+            return BadRequest("Failed to get employee details");
+        }
+
+        // POST: api/User/
+        [HttpPost(Name = "InsertEmployee")]
+        public ActionResult<IEnumerable<EmployeeItem>> InsertEmployee(EmployeeItem employee)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
             // return new string[]
-            return _context.GetEmployee(id);
+            return _context.InsertEmployee(employee);
+        }
+
+        // PUT: api/User/{id}
+        [HttpPut("{id}", Name = "UpdateEmployee")]
+        public ActionResult<IEnumerable<EmployeeItem>> UpdateEmployee(int id, EmployeeItem employee)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            // return new string[]
+            return _context.UpdateEmployee(id, employee);
+        }
+
+        // DELETE: api/User/{id}
+        [HttpDelete("{id}", Name = "DeleteEmployee")]
+        public ActionResult<IEnumerable<EmployeeItem>> DeleteEmployee(int id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            // return new string[]
+            if (_context.DeleteEmployee(id))
+            {
+                return Ok("Delete Success!");
+            }
+
+            return NotFound("Failed to delete employee record");
         }
     }
 }
--- Sesi09/Tugas09/webApiTest/Models/EmployeeContext.cs
+++ Sesi09/Tugas09/webApiTest/Models/EmployeeContext.cs
@@ -72,5 +72,102 @@ namespace webApiTest.Models
             }
             return list;
         }
+
+        public List<EmployeeItem> InsertEmployee(EmployeeItem employee)
+        {
+            List<EmployeeItem> list = new List<EmployeeItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(
+                    "INSERT INTO employee (name, gender, address) VALUES " +
+                    "(@name, @gender, @address)"
+                    , conn);
+                cmd.Parameters.AddWithValue("@name", employee.name);
+                cmd.Parameters.AddWithValue("@gender", employee.gender);
+                cmd.Parameters.AddWithValue("@address", employee.address);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new EmployeeItem(){
+                            id = reader.GetInt32("id"),
+                            name = reader.GetString("name"),
+                            gender = reader.GetString("gender"),
+                            address = reader.GetString("address")
+                        });
+                    }
+                }
+                conn.Close();
+            }
+
+            return list;
+        }
+
+        public List<EmployeeItem> UpdateEmployee(int id, EmployeeItem employee)
+        {
+            List<EmployeeItem> list = new List<EmployeeItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("UPDATE employee " +
+                    "SET name=@name, " +
+                    "gender=@gender, " +
+                    "address=@address " +
+                    "WHERE id=@id", conn);
+                cmd.Parameters.AddWithValue("@id", employee.id);
+                cmd.Parameters.AddWithValue("@name", employee.name);
+                cmd.Parameters.AddWithValue("@gender", employee.gender);
+                cmd.Parameters.AddWithValue("@address", employee.address);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new EmployeeItem()
+                        {
+                            id = reader.GetInt32("id"),
+                            name = reader.GetString("name"),
+                            gender = reader.GetString("gender"),
+                            address = reader.GetString("address")
+                        });
+                    }
+                }
+                conn.Close();
+            }
+
+            return list;
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            List<EmployeeItem> list = new List<EmployeeItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM employee WHERE id=@id", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                    return true;
+
+                } catch(Exception e)
+                {
+                    // Belom tau error handle nya mau gimana
+                    return false;
+                }
+                
+            }
+
+            return false;
+        }
     }
 }
